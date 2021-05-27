341018663a0f0704ee12e88c0170879bcf0ad4a2
Shahad Tariq <67611807+shtm7@users.noreply.github.com>
2021-05-28 00:03:30 +0300

    Add files via upload

---
+++ Employee-sh/database/migrations/2020_09_30_024957_create_employees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::defaultStringLength(191);
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('employee_name');
+            $table->Integer('employee_id')->unique();
+            $table->Integer('phone_number')->length(10)->unsigned();
+            $table->string('city');
+            $table->string('gender');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
